87e7cecc00a2a986d7e8933f4c68f5e82949b0e8
Michael Duerinckx <duerinckxmichael@gmail.com>
2014-10-12 18:09:07 +0100

    Code style cleanup, tweaks to adjustOutputVoltage etc

--- arduino/psu_controller/psu_controller.ino
+++ arduino/psu_controller/psu_controller.ino
@@ -64,8 +64,6 @@
 // Measured voltage is voltage-divided to stay within the ADC_REFERENCE VOLTAGE
 #define VOLTAGE_MEASURING_MULTIPLIER 9.4
 
-
-
 // For the main loop()
 #define LOOP_CYCLE_DELAY 200
 
@@ -101,9 +99,8 @@
 #define PWMO_VOLTAGE_COARSE 5
 #define PWMO_VOLTAGE_FINE   6
 
-enum displayMode 
-{
-  measurement,
+enum displayMode {
+  measuring,
   setting
 };
 
@@ -130,8 +127,8 @@ float voltageMeasurement;
 float currentMeasurement;
 
 
-displayMode voltageDisplayMode = measurement;
-displayMode currentDisplayMode = measurement;
+displayMode voltageDisplayMode = measuring;
+displayMode currentDisplayMode = measuring;
 
 // Remaining time before display goes back to measurement mode when in setting mode
 int voltageDisplayModeTimeout = 0;
@@ -151,8 +148,7 @@ void readInputs() {
   voltageMeasurement = readVoltage();
 }
 
-void setup() {
-  
+void setup() {  
   // Initialize rotary encoders
   voltageKnob = new ClickEncoder(
     I_ROTA_VOLTAGE,
@@ -196,33 +192,39 @@ void loop() {
   int currentKnobDiff = (int)(currentKnob->getValue() / UNITS_PER_ENCODER_NOTCH);
   
   // Voltage knob moved
-  if (voltageKnobDiff != 0)
-  {    
+  if (voltageKnobDiff != 0) {    
     voltageDisplayMode = setting; 
     adjustTargetVoltage(voltageKnobDiff);
     voltageDisplayModeTimeout = SETTING_DISP_TIMEOUT;
   }
   
   // Voltage knob button clicked
-  if (voltageKnob->getButton() == ClickEncoder::Clicked)
-  {
-    voltageDisplayMode = setting;
-    voltageDisplayModeTimeout = SETTING_DISP_TIMEOUT;
+  if (voltageKnob->getButton() == ClickEncoder::Clicked) {
+    if (voltageDisplayMode == setting) {
+      voltageDisplayMode = measuring;
+      voltageDisplayModeTimeout = 0;
+    } else {
+      voltageDisplayMode = setting;
+      voltageDisplayModeTimeout = SETTING_DISP_TIMEOUT;
+    }
   }
   
   // Current knob moved
-  if (currentKnobDiff != 0)
-  {
+  if (currentKnobDiff != 0) {
     currentDisplayMode = setting;
     adjustTargetCurrentLimit(currentKnobDiff);
     currentDisplayModeTimeout = SETTING_DISP_TIMEOUT;
   }
   
   // Current knob button clicked
-  if (currentKnob->getButton() == ClickEncoder::Clicked)
-  {
-    currentDisplayMode = setting;
-    currentDisplayModeTimeout = SETTING_DISP_TIMEOUT;
+  if (currentKnob->getButton() == ClickEncoder::Clicked) {
+    if (currentDisplayMode == setting) {
+      currentDisplayMode = measuring;
+      currentDisplayModeTimeout = 0;
+    } else {
+      currentDisplayMode = setting;
+      currentDisplayModeTimeout = SETTING_DISP_TIMEOUT;
+    }
   }
   
   updateDisplays();
@@ -235,8 +237,7 @@ void loop() {
  *
  * Constrains the target voltage to a minimum and maximum
  */
-void adjustTargetVoltage(int steps)
-{
+void adjustTargetVoltage(int steps) {
   targetVoltage += (steps * VOLTAGE_STEP);
   if (targetVoltage < VOLTAGE_TARGET_MINIMUM) targetVoltage = VOLTAGE_TARGET_MINIMUM;
   if (targetVoltage > VOLTAGE_TARGET_MAXIMUM) targetVoltage = VOLTAGE_TARGET_MAXIMUM;
@@ -247,8 +248,7 @@ void adjustTargetVoltage(int steps)
  *
  * Constrains the target current to a minimum and maximum
  */
-void adjustTargetCurrentLimit(int steps)
-{
+void adjustTargetCurrentLimit(int steps) {
   targetCurrentLimit += (steps * CURRENT_STEP);
   if (targetCurrentLimit < CURRENT_TARGET_MINIMUM) targetCurrentLimit = CURRENT_TARGET_MINIMUM;
   if (targetCurrentLimit > CURRENT_TARGET_MAXIMUM) targetCurrentLimit = CURRENT_TARGET_MAXIMUM;
@@ -270,7 +270,7 @@ void adjustTargetCurrentLimit(int steps)
 float readVoltage() {
   int inputValue = analogRead(AI_MEASURE_VOLTAGE);
   
-  float resultingVoltage = (
+  return (
       (
         (float) inputValue /
         (ADC_PRECISION / ADC_REFERENCE_VOLTAGE)
@@ -278,8 +278,6 @@ float readVoltage() {
       VOLTAGE_MEASURING_MULTIPLIER // That is, voltage divider ratio
     ) -
     currentMeasurement * CURRENT_MEASURING_RESISTANCE;
-    
-  return resultingVoltage;
 }
 
 /**
@@ -294,27 +292,31 @@ float readVoltage() {
 float readCurrent() {
   int inputValue = analogRead(AI_MEASURE_CURRENT);
   
-  float resultingCurrent = (           
+  return (           
       (float) inputValue / 
       (ADC_PRECISION / ADC_REFERENCE_VOLTAGE) 
     ) / 
     CURRENT_MEASURING_RESISTANCE;
-    
-  return resultingCurrent;
 }
 
 /**
  * Intermediate function for writeVoltage, adjusts voltage for current limiting
  */
-void adjustOutput()
-{
+void adjustOutput() {
   float outputVoltage = targetVoltage;
   float currentMeasuringVoltageDrop = (float) (currentMeasurement * CURRENT_MEASURING_RESISTANCE);
   float estimatedLoadResistance = voltageMeasurement / currentMeasurement;
   
-  if (targetCurrentLimit - currentMeasurement < -CURRENT_STEP)
-  {
-      outputVoltage = (targetCurrentLimit * estimatedLoadResistance);
+  // Adjust for current limit 
+  if (targetCurrentLimit - currentMeasurement < -CURRENT_STEP) {
+      outputVoltage = (targetCurrentLimit * estimatedLoadResistance) + 
+                      (targetCurrentLimit * CURRENT_MEASURING_RESISTANCE);
+  } else {
+    // Adjust for current measuring resistor voltage drop
+    // This might need tweaking depending on stability of the analog input measurements
+    if (targetVoltage - voltageMeasurement > VOLTAGE_STEP || currentMeasuringVoltageDrop > VOLTAGE_STEP) {
+      outputVoltage += currentMeasuringVoltageDrop;
+    }
   }
   writeVoltage(outputVoltage);
 }
@@ -324,30 +326,24 @@ void adjustOutput()
  * Sets the control voltage, connected to voltage adjust pins on
  * regulators.
  */
-void writeVoltage(float voltage)
-{
+void writeVoltage(float voltage) {
   voltage -= (float) REGULATOR_VOLTAGE_OFFSET;
    
   int coarse = (int)voltage;
   float fine;
-  if (coarse > COARSE_VOLTAGE_RANGE)
-  {
-    coarse -= (coarse - COARSE_VOLTAGE_RANGE);
-  }
+  if (coarse > COARSE_VOLTAGE_RANGE) coarse -= (coarse - COARSE_VOLTAGE_RANGE);
   
   fine = voltage - (float) coarse;
   writeCoarseVoltage(coarse);
   writeFineVoltage(fine);
 }
 
-void writeCoarseVoltage(int cVoltage)
-{
+void writeCoarseVoltage(int cVoltage) {
   int cAnalog = (int) ((PWM_PRECISION / (float) COARSE_VOLTAGE_RANGE) * cVoltage);
   analogWrite(PWMO_VOLTAGE_COARSE, cAnalog);  
 }
 
-void writeFineVoltage(float fVoltage)
-{
+void writeFineVoltage(float fVoltage) {
   int fAnalog = (int) ((PWM_PRECISION / (float) FINE_VOLTAGE_RANGE) * fVoltage);
   analogWrite(PWMO_VOLTAGE_FINE, fAnalog);
 }
@@ -364,70 +360,50 @@ void writeFineVoltage(float fVoltage)
  *   - Blinks the display out every 5 times this is called
  *   - Reverts to measurement mode after a timeout
  */
-void updateDisplays()
-{
+void updateDisplays() {
   // Voltage display mode control
-  if (voltageDisplayMode == setting)
-  {
+  if (voltageDisplayMode == setting) {
     voltageDisplayModeTimeout -= LOOP_CYCLE_DELAY;
-    if (voltageDisplayModeTimeout <= 0)
-    {
+    if (voltageDisplayModeTimeout <= 0) {
       voltageDisplayModeTimeout = 0;
-      voltageDisplayMode = measurement;
+      voltageDisplayMode = measuring;
     }
   }
   
-  // Current display mode control0
-  if (currentDisplayMode == setting)
-  {
+  // Current display mode control
+  if (currentDisplayMode == setting) {
     currentDisplayModeTimeout -= LOOP_CYCLE_DELAY;
-    if (currentDisplayModeTimeout <= 0)
-    {
+    if (currentDisplayModeTimeout <= 0) {
       currentDisplayModeTimeout = 0;
-      currentDisplayMode = measurement;
+      currentDisplayMode = measuring;
     }
   }
   
   // Voltage display
-  if (voltageDisplayMode == setting)
-  {
-    if (displayCycleStep == 0)
-    {
+  if (voltageDisplayMode == setting) {
+    if (displayCycleStep == 0) {
       clearDisplay(VOLTAGE_DISP_OFFSET);
-    }
-    else
-    {
+    } else {
       displayVoltage(targetVoltage);
     }
-  }
-  else
-  {
+  } else {
     displayVoltage(voltageMeasurement);
   }
   
   // Current display
-  if (currentDisplayMode == setting)
-  {
-    if (displayCycleStep == 0)
-    {
+  if (currentDisplayMode == setting) {
+    if (displayCycleStep == 0) {
       clearDisplay(CURRENT_DISP_OFFSET);
-    }
-    else
-    {
+    } else {
       displayCurrent(targetCurrentLimit);
     }
-  }
-  else
-  {
+  } else {
     displayCurrent(currentMeasurement);
   }
   
   // Advance display cycle
   displayCycleStep += 1;
-  if (displayCycleStep == DISPLAY_CYCLES)
-  {
-    displayCycleStep = 0;
-  }
+  if (displayCycleStep == DISPLAY_CYCLES) displayCycleStep = 0;
 }
 
 /**
@@ -437,8 +413,7 @@ void updateDisplays()
  * @todo Fix bug where first digit remains when no update is available
  *       Should be blanked instead.
  */
-void displayVoltage(float voltage)
-{
+void displayVoltage(float voltage) {
   float roundedVoltage = roundToNearest(voltage, VOLTAGE_STEP);
   signed char digits[DISP_DIGITS];
   prepareForDisplay(roundedVoltage, VOLTAGE_DECIMAL_POINTS, digits);
@@ -450,22 +425,24 @@ void displayVoltage(float voltage)
  *
  * Display it in format X.XXX
  */
-void displayCurrent(float current) 
-{
+void displayCurrent(float current) {
   float roundedCurrent = roundToNearest(current, CURRENT_STEP);
   signed char digits[DISP_DIGITS];
   prepareForDisplay(roundedCurrent, CURRENT_DECIMAL_POINTS, digits);
   writeDisplay(CURRENT_DISP_OFFSET, CURRENT_DECIMAL_POINTS, digits);
 }
 
-void writeDisplay(char displayOffset, char decimalPoints, signed char* digits)
-{
+void writeDisplay(char displayOffset, char decimalPoints, signed char* digits) {
   char i;
   disp.shutdown(0, true);
   
-  for(i = 0; i < DISP_DIGITS; i += 1)
-  {     
-    disp.setDigit(0, i + displayOffset, (digits[i] == -1 ? ' ' : digits[i]), i == DISP_DIGITS - (decimalPoints + 1));
+  for(i = 0; i < DISP_DIGITS; i += 1) {     
+    disp.setDigit(
+      0,
+      i + displayOffset,
+      (digits[i] == -1 ? ' ' : digits[i]),
+      i == DISP_DIGITS - (decimalPoints + 1)
+    );
   }
   
   disp.shutdown(0, false);
@@ -474,20 +451,14 @@ void writeDisplay(char displayOffset, char decimalPoints, signed char* digits)
 /**
  * Clear a single display by writing space characters to every digit
  */
-void clearDisplay(char displayOffset)
-{
+void clearDisplay(char displayOffset) {
   char i;
   disp.shutdown(0, true);
   
-  for(i = 0; i < DISP_DIGITS; i++)
-  {
-    disp.setDigit(0, i + displayOffset, ' ', false);
-  }
+  for(i = 0; i < DISP_DIGITS; i++) disp.setDigit(0, i + displayOffset, ' ', false);
   
   disp.shutdown(0, false);
 }
-
-// Below stuff has been tested with gcc on a PC
 
 /**
  * Rounds a float value to the nearest <rounding>
@@ -498,8 +469,7 @@ void clearDisplay(char displayOffset)
  * @todo Investigate using some stdlib stuff to make this neater,
  *       yet still not riddled with floating point errors.
  */
-float roundToNearest(float input, float rounding)
-{
+float roundToNearest(float input, float rounding) {
   int preMultiplier = 1;
   int intVersion;
 
@@ -515,10 +485,7 @@ float roundToNearest(float input, float rounding)
   int roundingLeftOver = (int)((float)remainderInt / (rounding * preMultiplier));
   float roundingRemainder = ((float)remainderInt / (rounding * preMultiplier)) - roundingLeftOver;
 
-  if (roundingRemainder >= ((rounding * preMultiplier) / 2))
-  {
-    roundingLeftOver += 1;
-  }
+  if (roundingRemainder >= ((rounding * preMultiplier) / 2)) roundingLeftOver += 1;
 
   int result = (intDivided * (rounding * preMultiplier)) + (roundingLeftOver * (rounding * preMultiplier)); 
   return (float)result / preMultiplier;
@@ -529,19 +496,14 @@ float roundToNearest(float input, float rounding)
  * 
  * Used for preparing to display float values on 7-segment displays
  */
-void prepareForDisplay(float input, char decimalPoints, signed char* outDigits)
-{
+void prepareForDisplay(float input, char decimalPoints, signed char* outDigits) {
   int intInput = (int)(((int)(input * power(10, decimalPoints))) % power(10, DISP_DIGITS));
   char i;
   signed char digit;
-  for (i = DISP_DIGITS - 1; i >= 0; i--)
-  {
+  for (i = DISP_DIGITS - 1; i >= 0; i--) {
     digit = (signed char)(((intInput % power(10, i + 1)) - (intInput % power(10, i))) / power(10, i));
     // Don't display a digit at [X]x.xx if [X] is 0
-    if (digit == 0 && i == (decimalPoints + 1))
-    {
-      digit = -1;
-    }
+    if (digit == 0 && i == (decimalPoints + 1)) digit = -1;
     outDigits[DISP_DIGITS - i - 1] = digit;
   }
 }
@@ -549,22 +511,15 @@ void prepareForDisplay(float input, char decimalPoints, signed char* outDigits)
 /**
  * Integer implementation of pow
  */
-int power(int base, int exp)
-{
-  int i;
-  int result;
+int power(int base, int exp) {
+  int i, result;
 
-  if (exp == 0)
-  {
-    return 1;
-  }
+  if (exp == 0) return 1;
 
   result = base;
 
-  for (i = 1; i < exp; i++)
-  {
-    result = result * base;
-  }
+  for (i = 1; i < exp; i++) result *=  base;
 
   return result;
 }
+
